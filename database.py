5cabfd2be1fac3fadd25cbeed1cfe61e831e5d1b
Patricio Pardo <33754907+p4tricio@users.noreply.github.com>
2023-08-19 01:56:38 -0400

    se agrega variable de entorno

--- database.py
+++ database.py
@@ -1,9 +1,13 @@
 from sqlalchemy.orm import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 #engine = create_engine("postgresql://postgres:8690@localhost/bd-test-carga", echo = True)
-engine = create_engine("postgresql://dreamteam:cjsLndVJRCsRpkXuwIzmCQLvOIZoaEJ5@dpg-cjfgd7gcfp5c7398q260-a.oregon-postgres.render.com/appbdpostgresv1_kgvm", echo = True)
+engine = create_engine(os.getenv('DATABASE_URL'), echo = True)
 
 Base = declarative_base()
 
